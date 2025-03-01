f2d8ee408155578a4f68118cb3b629f8c2979361
inelika <dosanenlik@gmail.com>
2025-03-01 13:27:08 +0500

    Create methods.js

---
+++ methods.js
@@ -0,0 +1,22 @@
+let method1 = [1, 2, 3, 4, 5];
+method1.forEach(num => console.log(num + 3));
+
+
+let method2 = [1, 2, 3, 4, 5];
+let squared = method2.map(num => num * 2);
+console.log(squared);
+
+
+let method3 = [5, 2, 9, 1, 4];
+method3.sort((a, b) => a - b);
+console.log(method3);
+
+
+let method4 = [1, 2, 3, 4, 5, 6];
+let evenNumbers = method4.filter(num => num % 3 === 0);
+console.log(evenNumbers);
+
+
+let method5 = [1, 2, 3, 4, 5];
+let sum = method5.reduce((acc, num) => acc + num, 0);
+console.log(sum);
